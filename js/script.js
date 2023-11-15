5c280bd5679b159bde852c2d352ab843c79b26e0
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-15 16:08:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Define assets to preload
     const assetsToLoad = [
+        'wwwroot/assets/CowboyHead.gif',
         'wwwroot/assets/Song.m4a',
         'wwwroot/videos/SW1.mp4',
         'wwwroot/videos/SW2.mp4',
