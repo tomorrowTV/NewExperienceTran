1b952998ab6ac4ed807f952fee239c3bca7a2d0e
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-17 21:56:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,7 +82,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Start tranVideo when the loading screen disappears
         const tranVideo = document.getElementById('tranVideo');
-        tranVideo.play().catch(error => console.error('tranVideo playback error:', error.message));   
+        tranVideo.muted = true; // Mute tranVideo initially
+        tranVideo.play().catch(error => console.error('tranVideo playback error:', error.message));
+
+        // Unmute tranVideo after 1000 milliseconds (1 second)
+        setTimeout(() => {
+            tranVideo.muted = false;
+        }, 1000);
     });
 
     // Function to start the game
