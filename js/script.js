1fe8a84d1d3fbc1ab47f84a9b5dcf4a838fb1fa4
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-19 11:03:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -109,7 +109,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Create a separate audio element for 'tranVideo'
         const tranVideoAudio = new Audio('wwwroot/assets/TranVid.mov');
-        tranVideoAudio.muted = true;
 
         // Ensure tranVideoAudioInstance is created only once
         if (!tranVideoAudioInstance) {
