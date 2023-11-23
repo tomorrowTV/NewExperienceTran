324d7f3cd810385cb098f6c88b341862ea1fc57d
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-23 17:14:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const loadingBar = document.getElementById('loadingBar');
     const loadingScreen = document.getElementById('loadingBarContainer');
-    const loadingText = document.getElementById('loadingText');
+    const loadingText = document.getElementById('loadingText'); // Add this line to get the loading text element
 
     let currentVideoIndex = 0;
-    let tranAudio;
-    let tranVideo;
+    let videoPlaying = false;
+    let audioPlaying = false;
+    let audioStartTime = 0;
     let tranVideoAudioContext;
     const preloadedVideos = [];
-    let audioContext;
-    let audioStartTime = 0;
 
+    // Define assets to preload
     const assetsToLoad = [
         'wwwroot/assets/CowboyHead.gif',
         'wwwroot/assets/TranVid.mov',
@@ -27,8 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const preload = new createjs.LoadQueue();
     preload.setMaxConnections(5);
 
+    // Preload assets with progress tracking
     preload.loadManifest(assetsToLoad);
 
+    // Add an event listener for when each asset is loaded
     preload.on('fileload', function (event) {
         const asset = event.item.src;
 
@@ -41,67 +43,80 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (preloadedVideos.length === assetsToLoad.length - 1) {
+            // All videos are preloaded, hide loading bar and start the game
             loadingBar.style.display = 'none';
-            loadingScreen.style.display = 'none';  // Added this line to hide the loading screen
-            videoPlayerContainer.style.display = 'block';  // Added this line to show the video player container
             startGame();
         }
     });
 
+    // Function to play video by index
     function playVideoByIndex(index) {
         const newVideo = preloadedVideos[index];
-        videoPlayerContainer.innerHTML = '';
+        videoPlayerContainer.innerHTML = ''; // Clear container
         videoPlayerContainer.appendChild(newVideo);
 
+        // Add the 'playsinline' attribute for mobile devices
         newVideo.setAttribute('playsinline', '');
+
+        // Set the current time in the video to match the audio start time
         newVideo.currentTime = audioStartTime;
 
+        console.log('Before play: audioStartTime =', audioStartTime);
+
+        // Add an event listener for when the video ends
         newVideo.addEventListener('ended', function () {
+            // Start the video over from the beginning
             newVideo.currentTime = 0;
             newVideo.play().catch(error => {
                 console.error('Video playback error:', error.message);
             });
         });
-
+        
         newVideo.play().catch(error => {
             console.error('Video playback error:', error.message);
         });
 
+        // Preload the next video while the current video is playing
         preloadNextVideo();
+        
+        console.log('After play');
     }
 
+    // Function to preload the next video in the array
     function preloadNextVideo() {
         const nextIndex = (currentVideoIndex + 1) % preloadedVideos.length;
         const nextVideo = preloadedVideos[nextIndex];
 
         if (!nextVideo.hasAttribute('src')) {
+            // Set the 'src' attribute to trigger preload
             nextVideo.src = preloadedVideos[nextIndex].src;
         }
     }
 
-    function startGame() {
-        playVideoByIndex(0);
-        loadingText.textContent = 'Click';
-    }
+    // Add an event listener for user clicks to switch videos
+    document.addEventListener('click', function () {
+        // Set the audio start time to match the current time in the current video
+        audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
 
-    function startAudioAndVideo() {
-        if (!tranAudio || tranAudio.getState() !== 'playFinished') {
+        // Preload the next video
+        preloadNextVideo();
+
+        // Switch to the next video
+        currentVideoIndex = (currentVideoIndex + 1) % preloadedVideos.length;
+        playVideoByIndex(currentVideoIndex);
+
+        // Start audio playback if not already playing
+        if (!audioPlaying) {
             createjs.Sound.registerSound({ src: 'wwwroot/assets/tranAudio.m4a', id: 'tranAudio' });
-            tranAudio = createjs.Sound.play('tranAudio', { loop: 0 });
+            const tranAudio = createjs.Sound.play('tranAudio', { loop: -1 });
+            audioPlaying = true;
 
-            if (!audioContext || audioContext.state !== 'running') {
-                audioContext = new (window.AudioContext || window.webkitAudioContext)();
-                audioContext.resume().then(() => {
-                    startTranVideo();
-                });
-            } else {
-                startTranVideo();
-            }
+            // Hide the loading screen when video starts playing
+            loadingScreen.style.display = 'none';
         }
-    }
-
-    function startTranVideo() {
-        tranVideo = document.getElementById('tranVideo');
+        
+        // Start tranVideo when the loading screen disappears
+        const tranVideo = document.getElementById('tranVideo');
         tranVideo.muted = true;
 
         if (!tranVideoAudioContext || tranVideoAudioContext.state !== 'running') {
@@ -112,17 +127,14 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             tranVideo.play().catch(error => console.error('tranVideo playback error:', error.message));
         }
-
-        tranVideo.addEventListener('ended', function () {
-            audioStartTime = 0;
-            startAudioAndVideo();
-        });
-    }
-
-    document.addEventListener('click', function () {
-        audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
-        preloadNextVideo();
-        currentVideoIndex = (currentVideoIndex + 1) % preloadedVideos.length;
-        startAudioAndVideo();
     });
+
+    // Function to start the game
+    function startGame() {
+        // Start with the first video in the array
+        playVideoByIndex(0);
+
+        // Change loading text to "Click" when the game starts
+        loadingText.textContent = 'Click';
+    }
 });
