4f6c737c5b5fdd369cb778162b1c9a816561fada
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-12-04 14:19:30 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const loadingBar = document.getElementById('loadingBar');
     const loadingScreen = document.getElementById('loadingBarContainer');
-    const loadingText = document.getElementById('loadingText'); // Add this line to get the loading text element
+    const loadingText = document.getElementById('loadingText');
 
     let currentVideoIndex = 0;
-    let videoPlaying = false;
     let audioPlaying = false;
-    let audioStartTime = 0;
     let audioContext;
     let tranVideoAudioContext;
     const preloadedVideos = [];
-    let gameOver = false; // New flag to track the game state
+    let gameOver = false;
 
     // Define assets to preload
     const assetsToLoad = [
@@ -53,12 +51,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to play video by index
     function playVideoByIndex(index) {
-
         if (gameOver) {
             // If the game is over, do not play the video
             return;
         }
-        
+
         const newVideo = preloadedVideos[index];
         videoPlayerContainer.innerHTML = ''; // Clear container
         videoPlayerContainer.appendChild(newVideo);
@@ -67,9 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
         newVideo.setAttribute('playsinline', '');
 
         // Set the current time in the video to match the audio start time
-        newVideo.currentTime = audioStartTime;
-
-        console.log('Before play: audioStartTime =', audioStartTime);
+        newVideo.currentTime = 0;
 
         // Add an event listener for when the video ends
         newVideo.addEventListener('ended', function () {
@@ -79,15 +74,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Video playback error:', error.message);
             });
         });
-        
+
         newVideo.play().catch(error => {
             console.error('Video playback error:', error.message);
         });
 
         // Preload the next video while the current video is playing
         preloadNextVideo();
-        
-        console.log('After play');
     }
 
     // Function to preload the next video in the array
@@ -102,72 +95,94 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     document.addEventListener('click', function () {
-        audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
-        preloadNextVideo();
+        // Switch to the next video
         currentVideoIndex = (currentVideoIndex + 1) % preloadedVideos.length;
-        playVideoByIndex(currentVideoIndex);
 
+        // Set the audio start time to match the current time in the current video
+        const audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
+
+        // Preload the next video
+        preloadNextVideo();
+
+        // Start audio playback if not already playing
         if (!audioPlaying) {
+            // Use createjs.Sound.play with configuration to get the HTMLMediaElement
             createjs.Sound.registerSound({ src: 'wwwroot/assets/tranAudio.m4a', id: 'tranAudio' });
             const tranAudio = createjs.Sound.play('tranAudio', { interrupt: createjs.Sound.INTERRUPT_EARLY, volume: 1 });
             audioPlaying = true;
 
+            // Create a shared audio context if not already created
             if (!audioContext) {
                 audioContext = new (window.AudioContext || window.webkitAudioContext)();
             }
-            
+
             // Connect tranAudio to the shared audio context
             const audioSource = audioContext.createMediaElementSource(tranAudio);
             audioSource.connect(audioContext.destination);
 
             loadingScreen.style.display = 'none';
 
+            // Add an event listener for when tranAudio finishes
             tranAudio.addEventListener('complete', function () {
+                // End the game when tranAudio finishes
+                // You can add your logic here to handle the end of the game
                 console.log('Game over!');
                 gameOver = true;
 
+                // Display "Game Over" message on the screen
                 const gameOverMessage = document.createElement('div');
                 gameOverMessage.textContent = 'Coming Soon..';
-                gameOverMessage.style.fontSize = '75px';
-                gameOverMessage.style.fontFamily = 'Futura, sans-serif';
-                gameOverMessage.style.fontWeight = 'bold';
-                gameOverMessage.style.color = '#eab5ac';
-                gameOverMessage.style.textAlign = 'center';
-                gameOverMessage.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.7)';
+                gameOverMessage.style.fontSize = '75px'; // Adjust styling as needed
+                gameOverMessage.style.fontFamily = 'Futura, sans-serif'; // Adjust font family as needed
+                gameOverMessage.style.fontWeight = 'bold'; // Adjust font weight as needed
+                gameOverMessage.style.color = '#eab5ac'; // Adjust font color as needed
+                gameOverMessage.style.textAlign = 'center'; // Center-align the text
+                gameOverMessage.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.7)'; // Add a simple text shadow
                 gameOverMessage.style.position = 'absolute';
                 gameOverMessage.style.top = '50%';
                 gameOverMessage.style.left = '50%';
                 gameOverMessage.style.transform = 'translate(-50%, -50%)';
-                gameOverMessage.style.zIndex = '1000';
+                gameOverMessage.style.zIndex = '1000'; // Set the z-index to a high value
                 document.body.appendChild(gameOverMessage);
             });
         }
 
+        // Start tranVideo when the loading screen disappears
         const tranVideo = document.getElementById('tranVideo');
         tranVideo.muted = true;
 
+        // Create a shared video audio context if not already created
         if (!tranVideoAudioContext) {
             tranVideoAudioContext = new (window.AudioContext || window.webkitAudioContext)();
         }
 
+        // Connect tranVideo to the shared audio context
         const videoSource = tranVideoAudioContext.createMediaElementSource(tranVideo);
         videoSource.connect(tranVideoAudioContext.destination);
 
+        // Add an event listener for when tranVideo finishes
         tranVideo.addEventListener('ended', function () {
             tranVideo.style.display = 'none';
         });
 
+        // Add an event listener for when tranVideo finishes loading
         tranVideo.addEventListener('loadeddata', function () {
+            // tranVideo has finished loading, start audio and video playback
             startTranAudio();
             tranVideo.play().catch(error => console.error('tranVideo playback error:', error.message));
         });
     });
 
+    // Function to start the game
     function startGame() {
+        // Start with the first video in the array
         playVideoByIndex(0);
+
+        // Change loading text to "Click" when the game starts
         loadingText.textContent = 'Click';
     }
 
+    // Function to start tranAudio and resume the audio context if needed
     function startTranAudio() {
         if (tranVideoAudioContext && tranVideoAudioContext.state !== 'running') {
             tranVideoAudioContext.resume().then(() => {
