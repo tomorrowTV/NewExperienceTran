aec26aa4613ffa467c5b1b90d2e2595b3d945a99
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-12-11 22:04:56 -0500

    fade in out attempt 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () { 
+document.addEventListener('DOMContentLoaded', function () {
     const tranVideo = document.getElementById('tranVideo');
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const loadingBar = document.getElementById('loadingBar');
     const loadingScreen = document.getElementById('loadingBarContainer');
-    const loadingText = document.getElementById('loadingText'); // Add this line to get the loading text element
+    const loadingText = document.getElementById('loadingText');
 
     let currentVideoIndex = 0;
     let videoPlaying = false;
@@ -11,9 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let audioStartTime = 0;
     let tranVideoAudioContext;
     const preloadedVideos = [];
-    let gameOver = false; // New flag to track the game state
+    let gameOver = false;
 
-    // Define assets to preload
     const assetsToLoad = [
         'wwwroot/assets/CowboyHead.gif',
         'wwwroot/assets/TranVid.mov',
@@ -29,10 +28,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const preload = new createjs.LoadQueue();
     preload.setMaxConnections(5);
 
-    // Preload assets with progress tracking
     preload.loadManifest(assetsToLoad);
 
-    // Add an event listener for when each asset is loaded
     preload.on('fileload', function (event) {
         const asset = event.item.src;
 
@@ -45,7 +42,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (preloadedVideos.length === assetsToLoad.length - 1) {
-            // All videos are preloaded, hide loading bar and start the game
             loadingBar.style.display = 'none';
             startGame();
         }
@@ -53,41 +49,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to play video by index
     function playVideoByIndex(index) {
-
         if (gameOver) {
-            // If the game is over, do not play the video
             return;
         }
-        
+
         const newVideo = preloadedVideos[index];
-        videoPlayerContainer.innerHTML = ''; // Clear container
+        const currentVideo = videoPlayerContainer.querySelector('video');
+
+        if (currentVideo) {
+            currentVideo.classList.add('fade-out');
+        }
+
+        videoPlayerContainer.innerHTML = '';
+        newVideo.classList.add('fade-in');
+
         videoPlayerContainer.appendChild(newVideo);
 
-        // Add the 'playsinline' attribute for mobile devices
         newVideo.setAttribute('playsinline', '');
-
-        // Set the current time in the video to match the audio start time
         newVideo.currentTime = audioStartTime;
 
-        console.log('Before play: audioStartTime =', audioStartTime);
-
-        // Add an event listener for when the video ends
         newVideo.addEventListener('ended', function () {
-            // Start the video over from the beginning
             newVideo.currentTime = 0;
             newVideo.play().catch(error => {
                 console.error('Video playback error:', error.message);
             });
         });
-        
+
         newVideo.play().catch(error => {
             console.error('Video playback error:', error.message);
         });
 
-        // Preload the next video while the current video is playing
         preloadNextVideo();
-        
-        console.log('After play');
+
+        setTimeout(() => {
+            newVideo.classList.remove('fade-in');
+        }, 100);
     }
 
     // Function to preload the next video in the array
@@ -96,59 +92,44 @@ document.addEventListener('DOMContentLoaded', function () {
         const nextVideo = preloadedVideos[nextIndex];
 
         if (!nextVideo.hasAttribute('src')) {
-            // Set the 'src' attribute to trigger preload
             nextVideo.src = preloadedVideos[nextIndex].src;
         }
     }
 
-    // Add an event listener for user clicks to switch videos
     document.addEventListener('click', function () {
-
-        // Set the audio start time to match the current time in the current video
         audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
-
-        // Preload the next video
         preloadNextVideo();
-
-        // Switch to the next video
         currentVideoIndex = (currentVideoIndex + 1) % preloadedVideos.length;
         playVideoByIndex(currentVideoIndex);
 
-        // Start audio playback if not already playing
         if (!audioPlaying) {
             createjs.Sound.registerSound({ src: 'wwwroot/assets/tranAudio.m4a', id: 'tranAudio' });
             const tranAudio = createjs.Sound.play('tranAudio');
             audioPlaying = true;
 
-            // Hide the loading screen when video starts playing
             loadingScreen.style.display = 'none';
 
-            // Add an event listener for when tranAudio finishes
             tranAudio.addEventListener('complete', function () {
-                // End the game when tranAudio finishes
-                // You can add your logic here to handle the end of the game
                 console.log('Game over!');
                 gameOver = true;
 
-                // Display "Game Over" message on the screen
                 const gameOverMessage = document.createElement('div');
                 gameOverMessage.textContent = 'Coming Soon..';
-                gameOverMessage.style.fontSize = '75px'; // Adjust styling as needed
-                gameOverMessage.style.fontFamily = 'Futura, sans-serif'; // Adjust font family as needed
-                gameOverMessage.style.fontWeight = 'bold'; // Adjust font weight as needed
-                gameOverMessage.style.color = '#eab5ac'; // Adjust font color as needed
-                gameOverMessage.style.textAlign = 'center'; // Center-align the text
-                gameOverMessage.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.7)'; // Add a simple text shadow
+                gameOverMessage.style.fontSize = '75px';
+                gameOverMessage.style.fontFamily = 'Futura, sans-serif';
+                gameOverMessage.style.fontWeight = 'bold';
+                gameOverMessage.style.color = '#eab5ac';
+                gameOverMessage.style.textAlign = 'center';
+                gameOverMessage.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.7)';
                 gameOverMessage.style.position = 'absolute';
                 gameOverMessage.style.top = '50%';
                 gameOverMessage.style.left = '50%';
                 gameOverMessage.style.transform = 'translate(-50%, -50%)';
-                gameOverMessage.style.zIndex = '1000'; // Set the z-index to a high value
+                gameOverMessage.style.zIndex = '1000';
                 document.body.appendChild(gameOverMessage);
             });
         }
-        
-        // Start tranVideo when the loading screen disappears
+
         const tranVideo = document.getElementById('tranVideo');
         tranVideo.muted = true;
         tranVideo.autoplay = true;
@@ -162,18 +143,13 @@ document.addEventListener('DOMContentLoaded', function () {
             tranVideo.play().catch(error => console.error('tranVideo playback error:', error.message));
         }
 
-        // Add an event listener for when tranVideo finishes
         tranVideo.addEventListener('ended', function () {
             tranVideo.style.display = 'none';
         });
     });
 
-    // Function to start the game
     function startGame() {
-        // Start with the first video in the array
         playVideoByIndex(0);
-
-        // Change loading text to "Click" when the game starts
         loadingText.textContent = 'Click';
     }
 });
